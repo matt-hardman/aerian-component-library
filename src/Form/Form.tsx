f3b674f4caefced4dc983a76a4a3c96fca69a181
Matt Hardman <matt.hardman@aerian.com>
2020-07-06 13:15:28 +0100

    remove unused classes

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -2,16 +2,13 @@ import React, { HTMLAttributes } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers";
-import cx from "classnames";
 
 import { FormContext } from "./useFormContext";
 
-import styles from "./Form.module.scss";
-
 interface FormProps extends HTMLAttributes<HTMLElement> {
-    validationSchema: Record<string, YupTypes>;
-    onSubmitFn: (data: Record<string, string | string[]>) => void;
-    onResetFn?: () => void;
+  validationSchema: Record<string, YupTypes>;
+  onSubmitFn: (data: Record<string, string | string[]>) => void;
+  onResetFn?: () => void;
 }
 
 type YupTypes = yup.StringSchema<string> | yup.ObjectSchema<object>;
@@ -19,59 +16,59 @@ type YupTypes = yup.StringSchema<string> | yup.ObjectSchema<object>;
 interface FormContentsProps extends HTMLAttributes<HTMLElement> {}
 
 interface FormControlsProps extends HTMLAttributes<HTMLElement> {
-    submitButtonText?: string;
-    resetButtonText?: string;
+  submitButtonText?: string;
+  resetButtonText?: string;
 }
 
 export const FormContents: React.FC<FormContentsProps> = ({
-    children,
-    className,
-    ...rest
+  children,
+  className,
+  ...rest
 }) => (
-    <div className={cx([styles.formContents, className])} {...rest}>
-        {children}
-    </div>
+  <div className={className} {...rest}>
+    {children}
+  </div>
 );
 
 export const FormControls: React.FC<FormControlsProps> = ({
-    className,
-    submitButtonText = "Submit",
-    resetButtonText = "Clear",
-    ...rest
+  className,
+  submitButtonText = "Submit",
+  resetButtonText = "Clear",
+  ...rest
 }) => (
-    <div className={cx([styles.formControls, className])} {...rest}>
-        <button type="submit">{submitButtonText}</button>
+  <div className={className} {...rest}>
+    <button type="submit">{submitButtonText}</button>
 
-        <button type="reset">{resetButtonText}</button>
-    </div>
+    <button type="reset">{resetButtonText}</button>
+  </div>
 );
 
 export function Form({
-    children,
-    className,
-    onSubmitFn,
-    onResetFn,
-    validationSchema,
-    ...rest
+  children,
+  className,
+  onSubmitFn,
+  onResetFn,
+  validationSchema,
+  ...rest
 }: FormProps) {
-    const wrappedValidationSchema = yup.object().shape(validationSchema);
+  const wrappedValidationSchema = yup.object().shape(validationSchema);
 
-    const { register, handleSubmit, control, errors } = useForm({
-        resolver: yupResolver(wrappedValidationSchema),
-    });
+  const { register, handleSubmit, control, errors } = useForm({
+    resolver: yupResolver(wrappedValidationSchema),
+  });
 
-    return (
-        <FormContext.Provider value={{ register, errors, control }}>
-            <form
-                onSubmit={handleSubmit(onSubmitFn)}
-                onReset={onResetFn}
-                className={cx([styles.form, className])}
-                {...rest}
-            >
-                {children}
-            </form>
-        </FormContext.Provider>
-    );
+  return (
+    <FormContext.Provider value={{ register, errors, control }}>
+      <form
+        onSubmit={handleSubmit(onSubmitFn)}
+        onReset={onResetFn}
+        className={className}
+        {...rest}
+      >
+        {children}
+      </form>
+    </FormContext.Provider>
+  );
 }
 
 export default Form;
