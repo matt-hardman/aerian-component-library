c3407c8b12d12a748e56a10b96ebabcf7271e68b
Matt Hardman <matt.hardman@aerian.com>
2020-07-09 11:25:19 +0100

    add ability to pass additional classname

--- src/Theme/index.tsx
+++ src/Theme/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import cx from "classnames";
 
 interface Options {
   className: string;
 }
 
-export const applyTheme = <T extends {}>(
+export const applyTheme = <T extends Options>(
   Component: React.FC<T>,
   { className }: Options
 ) => (props: T) => {
-  return <Component {...props} className={className} />;
+  return <Component {...props} className={cx([className, props.className])} />;
 };
 
 export default applyTheme;
